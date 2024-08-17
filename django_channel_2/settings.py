29d7dba13a3943e9a2994e65a4014520768d6d98
YASHSAXEN <yashsaxena210403@gmail.com>
2024-08-18 01:52:08 +0530

    update settings.py

--- django_channel_2/settings.py
+++ django_channel_2/settings.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = ['127.0.0.1','localhost','pokichat.onrender.com']
 
 INSTALLED_APPS = [
     'daphne',
-    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
